9126fd5782ea3eadb1fc62534fea41dc462b46e7
Belidhe <nagaraju.belidhe@capgemini.com>
2024-02-15 16:13:37 +0530

    terraformfiles

---
+++ provider.tf
@@ -0,0 +1,15 @@
+#provider.tf
+ 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+ 
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"  #change your region
+}
